897bc4d8dda6980ab25d6f409ed3fecb89322034
Jørgen Rieck <jurgemaister@gmail.com>
2012-03-07 13:27:09 +0100

    Added models for settings and modules

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307124547) do
+ActiveRecord::Schema.define(:version => 20120307124549) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -48,6 +48,20 @@ ActiveRecord::Schema.define(:version => 20120307124547) do
 
   create_table "groups", :force => true do |t|
     t.string   "members"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scmodules", :force => true do |t|
+    t.string   "name"
+    t.boolean  "active"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "settings", :force => true do |t|
+    t.string   "auth_module"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
